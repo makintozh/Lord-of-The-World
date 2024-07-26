91e3ded2166ccdfb35e3b640061d6a9a89274878
makintosh <mbpschhh@gmail.com>
2024-07-27 00:27:47 +0300

    Update Biom

--- src/scripts/game-scripts/BackGroundScrollBar.gd
+++
@@ -1,9 +0,0 @@
-extends VScrollBar
-
-#Камера2D
-@onready var camera_view = $"../../Camera2D"
-
-
-
-func _process(_delta):
-	camera_view.position.y = value #Вид Камеры = Значение Скроллинга
--- src/scripts/game-scripts/biom.gd
+++ src/scripts/game-scripts/biom.gd
@@ -2,8 +2,29 @@ extends Node2D
 
 
 
+#Камера2D
+@onready var camera_view = $Camera2D
+@onready var scrollbar = $BackGroundScrollBar
+
+
+
+
 #Если нажата стрелочка "Назад"
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/game-scenes/navigation-menu.tscn")
+
+
+
+
+
+func _on_back_ground_scroll_bar_value_changed(value):
+			camera_view.position.y = value   #Вид Камеры = Значение Скроллинга
+
+
+
+
+
+
+
 
 
