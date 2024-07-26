81e80dde87f6ae948f0baebed2189f20c6f8f8a1
makintosh <mbpschhh@gmail.com>
2024-07-27 01:33:39 +0300

    Added Scroll-Button

--- src/scripts/game-scripts/biom.gd
+++ src/scripts/game-scripts/biom.gd
@@ -4,7 +4,16 @@ extends Node2D
 
 #Камера2D
 @onready var camera_view = $Camera2D
-@onready var scrollbar = $BackGroundScrollBar
+
+@onready var top_button = $NaviagteBiom/TopButton/TopButtonContainer
+@onready var bottom_button = $NaviagteBiom/BottomButton/BottomButtonContainer
+
+
+var top_biom_position_y = 1450
+var bottom_biom_position_y = 453
+
+
+
 
 
 
@@ -17,14 +26,30 @@ func _on_back_button_pressed():
 
 
 
-func _on_back_ground_scroll_bar_value_changed(value):
-			camera_view.position.y = value   #Вид Камеры = Значение Скроллинга
+
+
+func _on_civil_pressed():
+	get_tree().change_scene_to_file("res://src/scenes/battle-scenes/civil-battle.tscn")
 
 
 
 
 
+func _on_top_button_pressed():
+	camera_view.position.y = 453
+	top_button.visible = false
+	bottom_button.visible = true
 
 
 
 
+func _on_bottom_button_pressed():
+	camera_view.position.y = 1450
+	top_button.visible = true
+	bottom_button.visible = false
+	
+	
+	
+	
+	
+	
