9b0eb5f1f177f2614ccaac0e93a54551396692c3
makintosh <mbpschhh@gmail.com>
2024-07-26 16:17:27 +0300

    Added Commentaries

--- src/scripts/game-scripts/battle.gd
+++ src/scripts/game-scripts/battle.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 
 
-
+#Если нажата стрелочка "Назад"
 func _on_back_button_pressed():
 	pass
--- src/scripts/game-scripts/biom.gd
+++ src/scripts/game-scripts/biom.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 
-
+#Если нажата стрелочка "Назад"
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/game-scenes/navigation-menu.tscn")
 
