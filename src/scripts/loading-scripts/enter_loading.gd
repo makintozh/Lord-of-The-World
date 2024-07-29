dd4864a3a0e4c5d0da0e1d4a51bf4cfcd132f1be
makintosh <mbpschhh@gmail.com>
2024-07-30 01:23:06 +0300

    Add Loading Scene

--- src/scripts/loading-scripts/enter_loading.gd
+++ src/scripts/loading-scripts/enter_loading.gd
@@ -1,7 +1,15 @@
 extends Control
 
+@onready var version = $Version/Version/Version
+@onready var gamename = $Name/Name/Name
+
 
 
 func _ready():
+	version.text = ProjectSettings.get_setting("application/config/version")
+	gamename.text = ProjectSettings.get_setting("application/config/name")
+
+
+
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://src/scenes/auth-scenes/login_ui.tscn")
