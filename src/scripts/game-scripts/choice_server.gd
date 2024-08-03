555ea2a9ab40795a9c5486772bd24b574e5e3ae4
makintosh <mbpschhh@gmail.com>
2024-08-04 00:11:23 +0300

    Added 12 Server

--- src/scripts/game-scripts/choice_server.gd
+++ src/scripts/game-scripts/choice_server.gd
@@ -6,6 +6,12 @@ extends Node2D
 @onready var server_4 = $"Server-List/#4 Server"
 @onready var server_5 = $"Server-List/#5 Server"
 @onready var server_6 = $"Server-List/#6 Server"
+@onready var server_7 = $"Server-List/#7 Server"
+@onready var server_8 = $"Server-List/#8 Server"
+@onready var server_9 = $"Server-List/#9 Server"
+@onready var server_10 = $"Server-List/#10 Server"
+@onready var server_11 = $"Server-List/#11 Server"
+@onready var server_12 = $"Server-List/#12 Server"
 
 
 @onready var server_1_name_label = $"Server-List/#1 Server/ServarNameContainer/ServerName"
@@ -14,6 +20,12 @@ extends Node2D
 @onready var server_4_name_label = $"Server-List/#4 Server/ServarNameContainer/ServerName"
 @onready var server_5_name_label = $"Server-List/#5 Server/ServarNameContainer/ServerName"
 @onready var server_6_name_label = $"Server-List/#6 Server/ServarNameContainer/ServerName"
+@onready var server_7_name_label = $"Server-List/#7 Server/ServarNameContainer/ServerName"
+@onready var server_8_name_label = $"Server-List/#8 Server/ServarNameContainer/ServerName"
+@onready var server_9_name_label = $"Server-List/#9 Server/ServarNameContainer/ServerName"
+@onready var server_10_name_label = $"Server-List/#10 Server/ServarNameContainer/ServerName"
+@onready var server_11_name_label = $"Server-List/#11 Server/ServarNameContainer/ServerName"
+@onready var server_12_name_label = $"Server-List/#12 Server/ServarNameContainer/ServerName"
 
 
 var server_1_address = null
@@ -22,9 +34,22 @@ var server_3_address = null
 var server_4_address = null
 var server_5_address = null
 var server_6_address = null
+var server_7_address = null
+var server_8_address = null
+var server_9_address = null
+var server_10_address = null
+var server_11_address = null
+var server_12_address = null
 
 
 @onready var refreshing = $Refresh
+
+
+@onready var camera_view = $CameraView
+
+
+@onready var to_second_page = $"To-Second-Page"
+@onready var to_first_page = $"To-First-Page"
 
 
 @onready var api = $APIRequest
@@ -51,6 +76,17 @@ func _on_api_request_request_completed(result, response_code, headers, body):
 	var api_response = JSON.parse_string(body.get_string_from_utf8())
 	var message = str(api_response["message"])
 	var servers = api_response["servers"]
+	#var servers = [
+		#{ "id": 1, "name": "#1 Alpha", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 2, "name": "#2 Beta", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 3, "name": "#3 Private", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 4, "name": "#4 Untested", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 5, "name": "#5 Unrecognized", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 6, "name": "#6 Makintosh", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 7, "name": "#7 Testing", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 8, "name": "#8 Defined", "locale": "RU", "address": "31.129.54.121" },
+		#{ "id": 9, "name": "#9 Lord", "locale": "RU", "address": "31.129.54.121" }]
+	
 	
 	
 	print("\nВсе Сервера в JSON: " + str(servers))
@@ -67,7 +103,7 @@ func _on_api_request_request_completed(result, response_code, headers, body):
 		
 		
 		for index in server_id:
-			
+			print("Индекс: " + str(index))
 			if "1" in index:
 				server_1.visible = true
 				server_1_name_label.text = server_name
@@ -109,6 +145,35 @@ func _on_api_request_request_completed(result, response_code, headers, body):
 				server_6_address = server_address
 				print(server_name)
 				print(server_address)
+				
+			elif "7" in index:
+				to_first_page.visible = true
+				to_second_page.visible = true
+				server_7.visible = true
+				server_7_name_label.text = server_name
+				server_7_address = server_address
+				print(server_name)
+				print(server_address)
+				
+			elif "8" in index:
+				server_8.visible = true
+				server_8_name_label.text = server_name
+				server_8_address = server_address
+				print(server_name)
+				print(server_address)
+				
+			elif "9" in index:
+				server_9.visible = true
+				server_9_name_label.text = server_name
+				server_9_address = server_address
+				print(server_name)
+				print(server_address)
+				
+				
+				
+			
+				
+				
 				
 			else:
 				printerr("Ошибка сервера")
@@ -174,6 +239,19 @@ func _on_play_button_6_server_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/game-scenes/character_create.tscn")
 
 
+
+
+
+
+
+func _on_to_second_page_pressed():
+	camera_view.position.x = 1185
+
+
+
+
+func _on_to_first_page_pressed():
+	camera_view.position.x = 252
 
 
 
